a74e862d15d0fc69ffb41c83f80aaab723aa91c9
Mauricio Molina <maupena9@gmail.com>
2023-08-20 12:20:04 -0500

    Consuming a maps API

---
+++ API MAPS/script.js
@@ -0,0 +1,11 @@
+function iniciarMap(){
+    var coord = {lat:10.9811017 ,lng: -74.7837371};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10.5,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+} 
